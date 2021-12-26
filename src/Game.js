466e97a99178a42c02f9eb995d9c762ea3407355
Daniel Ortega Fernández <96653610+ortegask1598@users.noreply.github.com>
2021-12-26 11:41:15 +0100

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default function Game(props) {
+    
     return (
     <>
     <div>{props.quiz.question}</div>
-    <img src={props.quiz.attachment.url} />
+    <img src={props.quiz.attachment.url ? props.quiz.attachment.url : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.teepublic.com%2Fes-mx%2Fpegatina%2F7791716-nani-anime-meme&psig=AOvVaw0kov2LyQzJTbzd9fCKadvK&ust=1640599821519000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjQkuKcgfUCFQAAAAAdAAAAABAD'} />
     <input  type="text" id="answer" placeholder="Answer"></input>
-    <img src={props.quiz.author.photo.url} />
+    <img src={props.quiz.author.photo.url ? props.quiz.author.photo.url : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.esquire.com%2Fes%2Factualidad%2Ftv%2Fg38201395%2Fmejores-animes-dibujos-japoneses-netflix%2F&psig=AOvVaw3Xfl6CS8JgQeDdwZ3f3V_X&ust=1640599716596000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjC2rWcgfUCFQAAAAAdAAAAABAE'} />
     <h1> {props.quiz.author.username} </h1>
     <button className="guardar" onClick={props.save}>Guardar mi respuesta</button>
     <button className="anterior" onClick={props.previous}>Anterior</button>
@@ -16,4 +17,4 @@ export default function Game(props) {
     </>
     );
 }
-    
+   
