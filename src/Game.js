41b38f0d67c6e60418c5b96450db26816f79179a
Daniel Ortega Fernández <96653610+ortegask1598@users.noreply.github.com>
2021-12-26 16:52:16 +0100

    Update Game.js

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 export default function Game(props) {
-    
+    //TODO condición para las fotos
     return (
     <>
     <div>{props.quiz.question}</div>
-    <img src={props.quiz.attachment.url ? props.quiz.attachment.url : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.teepublic.com%2Fes-mx%2Fpegatina%2F7791716-nani-anime-meme&psig=AOvVaw0kov2LyQzJTbzd9fCKadvK&ust=1640599821519000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJjQkuKcgfUCFQAAAAAdAAAAABAD'} />
+    <img src={(props.quiz.attachment) ? props.quiz.attachment.url : 'https://images5.memedroid.com/images/UPLOADED2/50dda4c4d2e08.jpeg'} />
     <input  type="text" id="answer" placeholder="Answer"></input>
-    <img src={props.quiz.author.photo.url ? props.quiz.author.photo.url : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.esquire.com%2Fes%2Factualidad%2Ftv%2Fg38201395%2Fmejores-animes-dibujos-japoneses-netflix%2F&psig=AOvVaw3Xfl6CS8JgQeDdwZ3f3V_X&ust=1640599716596000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCIjC2rWcgfUCFQAAAAAdAAAAABAE'} />
+    <img src={(props.quiz.author) ? props.quiz.author.photo.url : 'https://es.web.img3.acsta.net/r_654_368/newsv7/19/01/15/15/29/1148475.jpg'} />
     <h1> {props.quiz.author.username} </h1>
     <button className="guardar" onClick={props.save}>Guardar mi respuesta</button>
     <button className="anterior" onClick={props.previous}>Anterior</button>
@@ -17,4 +17,4 @@ export default function Game(props) {
     </>
     );
 }
-   
+    
